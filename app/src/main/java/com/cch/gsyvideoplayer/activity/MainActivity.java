9523a0be29e7c35d0987137f0e8f1065c0336157
chenchanghua <742226482@qq.com>
2019-12-27 10:35:05 +0800

    修改了MainActivity添加了注释2

--- app/src/main/java/com/cch/gsyvideoplayer/activity/MainActivity.java
+++ app/src/main/java/com/cch/gsyvideoplayer/activity/MainActivity.java
@@ -46,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
 
         //发送网络请求获取数据
         //qwrqwerqw
+        //cch的修改
         OkHttpUtils
                 .get()
                 .url(API.get_index_list)
