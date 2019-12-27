588d0c7c67ad3aeda19eee5800e037b26665bf9f
lushun <1720920833@qq.com>
2019-12-27 21:08:15 +0800

    主页初设计（1）

--- app/src/main/java/com/cch/gsyvideoplayer/fragment/RecommendFragment.java
+++ app/src/main/java/com/cch/gsyvideoplayer/fragment/RecommendFragment.java
@@ -1,6 +1,9 @@
 package com.cch.gsyvideoplayer.fragment;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -9,11 +12,18 @@ import android.view.ViewGroup;
 
 import com.cch.gsyvideoplayer.R;
 
+import butterknife.OnClick;
+
 public class RecommendFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_recomment, container, false);
         return view;
+    }
+    //点击打开频道界面
+    @OnClick(R.id.gengduo_1)
+    void zhuan1(){
+
     }
 }
